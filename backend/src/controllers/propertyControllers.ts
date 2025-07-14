ee39d8373a1d178b87d866623ee65b26e8940f7c
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2025-07-14 14:43:55 +0530

    add property creation route with Cloudinary image upload and Multer support

--- backend/src/controllers/propertyControllers.ts
+++ backend/src/controllers/propertyControllers.ts
@@ -1,7 +1,11 @@
 import { Request,Response } from "express";
 import { PrismaClient,Prisma } from "@prisma/client";
 import { wktToGeoJSON } from "@terraformer/wkt";
+import { File as MulterFile } from "multer";
 
+interface MulterRequest extends Request {
+  files: MulterFile[];
+}
 import { v2 as cloudinary } from 'cloudinary';
 import { Location } from "@prisma/client";
 import axios from "axios";
@@ -194,7 +198,8 @@ export const createProperty = async (
     res: Response
 ): Promise<void> => {
     try {
-        const files = req.files as Express.Multer.File[];
+        const files = (req as MulterRequest).files;
+
         const {
             address,
             city,
