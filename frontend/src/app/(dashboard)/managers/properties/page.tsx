3886a870b3132816f4492dd281bc1852f36e6517
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2025-08-03 22:59:01 +0530

    chore: scaffold dashboard layout and initial manager/tenant route structure

---
+++ frontend/src/app/(dashboard)/managers/properties/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+
+import { useGetAuthUserQuery, useGetManagerPropertiesQuery } from "@/state/api";
+import React from "react";
+
+const Properties = () => {
+  
+  
+  
+  return (
+    <div className="dashboard-container">
+      hi you are on manager deshboard
+    </div>
+  );
+};
+
+export default Properties;
